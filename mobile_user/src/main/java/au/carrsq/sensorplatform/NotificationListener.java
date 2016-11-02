27f868611f6baa455567b80c4a5cf0ebdd60552d
mnefzger <matthiasnefzger.mail@gmail.com>
2016-11-03 09:29:25 +1000

    Changed Label of service

--- mobile_user/src/main/java/au/carrsq/sensorplatform/NotificationListener.java
+++ mobile_user/src/main/java/au/carrsq/sensorplatform/NotificationListener.java
@@ -5,7 +5,7 @@ import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
 
-public class NotificationListener extends NotificationListenerService{
+public class NotificationListener extends NotificationListenerService {
 
     private final String TAG = "USER_NOTIFICATION";
 
